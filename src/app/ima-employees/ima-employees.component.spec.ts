6d8104754d05cd2f7464cb7262081ecead3c4857
Andreas <andreas.raith@edu.fh-joanneum.at>
2019-11-20 12:02:18 +0100

    Lab2 - Moar tests

--- src/app/ima-employees/ima-employees.component.spec.ts
+++ src/app/ima-employees/ima-employees.component.spec.ts
@@ -3,10 +3,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ImaEmployeesComponent } from './ima-employees.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import {AppComponent} from '../app.component';
+import {BehaviorSubject} from 'rxjs';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
+
+
 
 describe('ImaEmployeesComponent', () => {
   let component: ImaEmployeesComponent;
   let fixture: ComponentFixture<ImaEmployeesComponent>;
+  const params = new BehaviorSubject<any>({filter: ''});
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,7 +20,16 @@ describe('ImaEmployeesComponent', () => {
         ReactiveFormsModule,
         RouterTestingModule
       ],
-      declarations: [ ImaEmployeesComponent ]
+      declarations: [ ImaEmployeesComponent ],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          params,
+          snapshot: {
+            paramMap: convertToParamMap({filter: ''})
+          }
+        }
+      }]
     })
     .compileComponents();
   }));
@@ -27,5 +42,14 @@ describe('ImaEmployeesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`there should be 37 entries'`, () => {
+    expect(component.imaEmployeesArray.length).toBe(37);
+  });
+
+  it(`there should be 2 leiter'`, () => {
+    params.next({filter: 'leiter'});
+    expect(component.imaEmployeesArray.length).toBe(2);
   });
 });
